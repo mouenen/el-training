4f0ca5e829705e1ab366a4d9feeaac50463b63ea
mouenen <g1135236859@gmail.com>
2018-11-01 21:57:59 +0800

    タスクのモデルを作成する

---
+++ db/migrate/20181101072706_create_tasks.rb
@@ -0,0 +1,16 @@
+class CreateTasks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :tasks do |t|
+      t.datetime :created_at, null: false
+      t.datetime :finished_at, null: false
+      t.integer :sequence, null: false, default: 0 # 大数優先
+      t.integer :status, null: false, default: -1 # -1: 未着手, 0: 着手中, 1: 完了
+      t.string :title, null: false, default: '', limit: 30
+      t.text :content, null: false, default: ''
+
+      t.references :user, foreign_key: true, null: false
+
+      t.index %i[user_id], name: 'index_tasks_uniqueness', unique: true
+    end
+  end
+end
