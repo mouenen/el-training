9dbc3b0cb1fc2b7941fd34bbacb477bdba494a19
mouenen <g1135236859@gmail.com>
2018-11-02 17:39:26 +0800

    詳細画面(show)を作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
     @tasks = Task.all
+  end
+
+  def show
+    @page_title = @task.title
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
