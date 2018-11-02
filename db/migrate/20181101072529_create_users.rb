c41b2080d4ff7bfec47f383a8f5e7466ce8301f1
mouenen <g1135236859@gmail.com>
2018-11-02 10:53:22 +0800

    ユーザーモデルを再作成する

--- db/migrate/20181101072529_create_users.rb
+++ db/migrate/20181101072529_create_users.rb
@@ -2,7 +2,21 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name, null: false, limit: 20, default: ''
+      t.boolean :admin, null: false, default: false
+      t.timestamps
+
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+      t.datetime :remember_created_at
+
+      # アカウントをアクティブにする
+      t.string :activation_digest
+      t.boolean :activated, null: false, default: false
+      t.datetime :actived_at
       
+      # パスワードをリセットする
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
     end
   end
 end
