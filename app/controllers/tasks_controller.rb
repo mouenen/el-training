9d1ecdb37b2dc4eaf2e061d9fab3379c6b5602e9
mouenen <g1135236859@gmail.com>
2018-11-02 18:01:20 +0800

    作成画面(new)を作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,7 +9,24 @@ class TasksController < ApplicationController
     @page_title = @task.title
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render new_task_path
+    end
+  end
+
   private
+
+  def task_params
+    params.require(:task).permit(:title, :content, category_ids: [])
+  end
 
   def set_task
     @task = Task.find(params[:id])
