d16755fa9a156c77f0dfe131f165346a0c5f2a01
mouenen <g1135236859@gmail.com>
2018-11-09 13:55:05 +0800

    ログイン機能を作る

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
+  
+  def new
+  end
 
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:notice] = 'Welcome ' + current_user.name
+      redirect_to tasks_path
+    else
+      flash[:alert] = 'Invalid email/password combination.'
+      render 'new'
+    end
+  end
 end
