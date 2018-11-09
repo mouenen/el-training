e5b8764359ad3509f612a2bd42b8ccc8da20e338
mouenen <g1135236859@gmail.com>
2018-11-09 14:00:19 +0800

    routes.rbを再設定する

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
+  get '/signup', to: 'user#new'
   resources :users, :tasks, :categories
-  root 'tasks#index'
+  root 'sessions#new'
 end
