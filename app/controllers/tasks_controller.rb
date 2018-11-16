8f625ecd43eb256e5c2935055657da642f7370e2
mouenen <g1135236859@gmail.com>
2018-11-16 19:42:26 +0800

    タスクのtitle,finished_atを検証する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -42,7 +42,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :content, :sequence, category_ids: [])
+    params.require(:task).permit(:title, :content, :finished_at, category_ids: [])
   end
 
   def set_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,8 @@ class Task < ApplicationRecord
   belongs_to :user
   has_many :category_tasks, dependent: :destroy
   has_many :categories, through: :category_tasks
+  validates :user_id, presence: true
+  validates :title, presence: true, length: { minimum: 3, maximum: 30 }
+  VALID_DATETIME_REGEX = /\A([1-9]\d\d\d\-0?[1-9]|1[12]\-0?[1-9]|[12]\d|3[01]) ([01]?[1-9]|2[0-4]:[0-5]\d:[0-5]\d)\z/
+  validates :finished_at, presence: true, format: { with: VALID_DATETIME_REGEX }
 end
