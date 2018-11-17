8fc8f6815ae286d47f28d3368f46c394ed9ad032
mouenen <g1135236859@gmail.com>
2018-11-17 18:59:34 +0800

    ユーザの詳細画面を作成する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
 
   def new
     @user = User.new
@@ -19,5 +20,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
