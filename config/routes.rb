dcd1207c77bef998a8c1c149f1d5a4c1fb764131
mouenen <g1135236859@gmail.com>
2018-11-18 23:45:02 +0800

    letter_openerを設定する

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   resources :users, :tasks, :categories
   resources :account_activations, only: [:edit]
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   root 'sessions#new'
 end
