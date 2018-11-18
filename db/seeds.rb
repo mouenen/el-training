d16ceb42fb97bf843d23dbacb8185b3a5e3283ae
mouenen <g1135236859@gmail.com>
2018-11-18 23:39:11 +0800

    adminのactivatedとactivated_atを追加する

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,14 +9,6 @@ User.create!(name: 'admin',
              email: 'admin@example.com',
              password: 'foobar',
              password_confirmation: 'foobar',
-             admin: true)
-
-# 99.times do |n|
-#   name = Faker::Name.name
-#   email = "user-#{n+1}@example.com"
-#   password = 'password'
-#   User.create!(name: name,
-#                email: email,
-#                password: password,
-#                password_confirmation: password)
-# end
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
