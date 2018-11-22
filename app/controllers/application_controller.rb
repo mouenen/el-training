a1045fe400be6e0726a8cc35154230d4a5eb643d
mouenen <g1135236859@gmail.com>
2018-11-22 12:04:55 +0800

    application_controllerを修正する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,11 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def remember_user
-    if (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(:remember, cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
+    return unless cookies.signed[:user_id]
+    user = User.find_by(id: cookies.signed[:user_id])
+    if user && user.authenticated?(:remember, cookies[:remember_token])
+      log_in user
+      @current_user = user
     end
   end
 end
