1d32b1647f4ff1bd5e980a4f428d05cb7e2703d6
mouenen <g1135236859@gmail.com>
2018-11-22 10:03:18 +0800

    cookieでユーザを覚える

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,7 +4,6 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
-      log_in user
       flash[:notice] = "Account activated!"
       redirect_to user
     else
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  before_action :remember_user
+  skip_before_action :verify_authenticity_token
+
+  def remember_user
+    if (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 end
