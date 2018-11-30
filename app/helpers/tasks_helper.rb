05fe65f954ad5f18e89ee770de7143db1afc4bf8
mouenen <g1135236859@gmail.com>
2018-11-30 17:17:55 +0800

    タスクの一覧画面でurl表した問題を修正する

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,9 +1,9 @@
 module TasksHelper
-  def sort_link_helper(param)
+
+  def sort_link_helper(text, param)
     key = param
     key += '_reverse' if params[:sort] == param
-    options = { url: { action: 'sort', sort: key } }
-    html_options = { action: 'sort', sort: key }
-    link_to(options, html_options, class: 'glyphicon glyphicon-sort')
+    url = { action: 'sort', sort: key }
+    link_to(text, url, class: 'glyphicon glyphicon-sort')
   end
 end
